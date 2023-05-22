407195f4d00f59761441352dcf55cc4ee134bb3c
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-05-23 02:06:14 +0900

    ViewBinding 진행

--- app/src/main/java/kr/co/episode/epilepsee/MainActivity.java
+++ app/src/main/java/kr/co/episode/epilepsee/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import kr.co.episode.epilepsee.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
     }
 }
