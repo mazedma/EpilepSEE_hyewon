24cd082055b163b5bd3e2f219c5ec8cf20e1cf7b
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-05-23 05:47:05 +0900

    StatusMenuActivity.java 뒷배경 반투명화

--- app/src/main/java/kr/co/episode/epilepsee/StatusMenuActivity.java
+++ app/src/main/java/kr/co/episode/epilepsee/StatusMenuActivity.java
@@ -3,6 +3,8 @@ package kr.co.episode.epilepsee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import kr.co.episode.epilepsee.databinding.ActivityStatusMenuBinding;
 
@@ -14,5 +16,11 @@ public class StatusMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityStatusMenuBinding = ActivityStatusMenuBinding.inflate(getLayoutInflater());
         setContentView(activityStatusMenuBinding.getRoot());
+
+        //뒷배경이 반투명한 activity 만들기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
+                WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
     }
 }
