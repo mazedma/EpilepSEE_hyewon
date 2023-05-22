c499832e6e965958a2cc08e76f5a98328128b450
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-05-23 06:21:03 +0900

    popup_menu.xml 생성 및 버튼 연결

--- app/src/main/java/kr/co/episode/epilepsee/MainActivity.java
+++ app/src/main/java/kr/co/episode/epilepsee/MainActivity.java
@@ -3,6 +3,10 @@ package kr.co.episode.epilepsee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import kr.co.episode.epilepsee.databinding.ActivityMainBinding;
 
@@ -14,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
+
+        // menu popup 버튼 연결
+        activityMainBinding.menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+                getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu()); // menu xml에서 메뉴 리스트 가져오는듯
+
+                //아이템 클릭 리스너
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if (menuItem.getItemId() == R.id.menu_profile){
+                            Toast.makeText(MainActivity.this, "프로필 클릭", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "대발작 감지 기능", Toast.LENGTH_SHORT).show();
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+
+
     }
 }
