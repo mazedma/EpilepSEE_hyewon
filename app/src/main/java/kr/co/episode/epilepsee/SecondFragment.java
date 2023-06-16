fbda6817e00a6266db45b1b6932de21190877f3e
DESKTOP-T05A3CM\YINA <dlqueen16@sookmyung.ac.kr>
2023-06-16 21:06:33 +0900

    필수입력까지 완료

--- app/src/main/java/kr/co/episode/epilepsee/SecondFragment.java
+++ app/src/main/java/kr/co/episode/epilepsee/SecondFragment.java
@@ -1,4 +1,67 @@
 package kr.co.episode.epilepsee;
 
-public class SecondFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+public class SecondFragment extends Fragment {
+    private Button btnPartialSeizure;
+    private Button btnGeneralSeizure;
+    private boolean isPartialSeizureClicked = false;
+    private boolean isGeneralSeizureClicked = false;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_second, container, false);
+
+        TextView textView7 = rootView.findViewById(R.id.textView7);
+        textView7.setText("isPartialSeizureClicked: "+ (isPartialSeizureClicked ? "true" : "false"));
+
+        btnPartialSeizure = rootView.findViewById(R.id.btnPartialSeizure);
+
+        btnPartialSeizure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isPartialSeizureClicked = true;  // 부분 발작 버튼이 클릭되었음을 표시
+                isGeneralSeizureClicked = false;  // 전신 발작 버튼은 클릭되지 않았음을 표시
+                textView7.setText("isPartialSeizureClicked: "+ (isPartialSeizureClicked ? "true" : "false"));
+            }
+        });
+        btnGeneralSeizure = rootView.findViewById(R.id.btnGeneralSeizure);
+        btnGeneralSeizure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isPartialSeizureClicked = false;  // 부분 발작 버튼은 클릭되지 않았음을 표시
+                isGeneralSeizureClicked = true;  // 전신 발작 버튼이 클릭되었음을 표시
+                textView7.setText("isPartialSeizureClicked: "+ (isPartialSeizureClicked ? "true" : "false"));
+
+            }
+
+        });
+
+        return rootView;
+    }
+        public boolean isPartialSeizureClicked() {
+            return isPartialSeizureClicked;
+        }
+
+        public boolean isGeneralSeizureClicked() {
+            return isGeneralSeizureClicked;
+        }
+
+
+        //데이터 저장 함수 작성
 }
+
+
--- app/src/main/java/kr/co/episode/epilepsee/SeizureActivity.java
+++ app/src/main/java/kr/co/episode/epilepsee/SeizureActivity.java
@@ -3,17 +3,23 @@ package kr.co.episode.epilepsee;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import kr.co.episode.epilepsee.databinding.ActivitySeizureBinding;
@@ -21,11 +27,15 @@ import kr.co.episode.epilepsee.databinding.ActivitySeizureBinding;
 public class SeizureActivity extends AppCompatActivity {
 
     ActivitySeizureBinding activitySeizureBinding;
+    Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activitySeizureBinding = ActivitySeizureBinding.inflate(getLayoutInflater());
         setContentView(activitySeizureBinding.getRoot());
+
+        nextButton = activitySeizureBinding.nextButton;
 
         //액션바에 백 버튼 추가
         ActionBar actionBar = getSupportActionBar();
@@ -34,15 +44,71 @@ public class SeizureActivity extends AppCompatActivity {
         }
         getSupportActionBar().setTitle("발작 기록"); // 화면 제목 설정
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
-    }
 
-    // 액션바의 백 버튼 클릭 이벤트 처리
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment nextFragment;
+                switch (getCurrentFragmentIndex()) {
+                    case 0:
+                        nextFragment = new SecondFragment();
+                        break;
+                    case 1:
+                        SecondFragment secondFragment = (SecondFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+                            if (secondFragment.isPartialSeizureClicked()) {
+                                nextFragment = new FourthFragment();
+                            } else if (secondFragment.isGeneralSeizureClicked()) {
+                                nextFragment = new ThirdFragment();
+                            } else {
+                                return;
+                            }
 
+                        break;
+
+                    default:
+                        returnToHomeScreen();
+                        return;
+                }
+
+                replaceFragment(nextFragment);
+            }
+        });
     }
+        private int getCurrentFragmentIndex () {
+            // 현재 표시된 프래그먼트의 인덱스를 반환하는 로직을 작성해주세요.
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            List<Fragment> fragments = fragmentManager.getFragments();
+            int lastIndex = fragments.size() - 1;
+            for (int i = lastIndex; i >= 0; i--) {
+                Fragment fragment = fragments.get(i);
+                if (fragment != null && fragment.isVisible()) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void replaceFragment (Fragment fragment){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+
+            fragmentTransaction.commit();
+        }
+        private void returnToHomeScreen () {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+        // 액션바의 백 버튼 클릭 이벤트 처리
+        @Override
+        public boolean onSupportNavigateUp () {
+            onBackPressed();
+            return true;
+        }
+
+
 
 
 }
