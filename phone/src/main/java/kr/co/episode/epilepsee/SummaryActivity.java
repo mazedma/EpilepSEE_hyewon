6931dca827ac55aafb814234a12bc8a4754f00e1
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-06-20 11:22:19 +0900

    일단 여기까지 실행 잘됐음

--- phone/src/main/java/kr/co/episode/epilepsee/SummaryActivity.java
+++ phone/src/main/java/kr/co/episode/epilepsee/SummaryActivity.java
@@ -1,14 +1,61 @@
 package kr.co.episode.epilepsee;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import kr.co.episode.epilepsee.databinding.ActivitySummaryBinding;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    private ActivitySummaryBinding binding;
+    private Handler myHandler;
+    private int receivedMessageNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_summary);
+
+        binding = ActivitySummaryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        myHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Bundle stuff = msg.getData();
+                String messageText = stuff.getString("messageText");
+                messageText(messageText);
+                return true;
+            }
+        });
+
+        // Register to receive local broadcasts
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        Receiver messageReceiver = new Receiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
+    }
+
+    public void messageText(String newInfo) {
+        if (newInfo != null && !newInfo.isEmpty()) {
+            binding.textView18.append("\n" + newInfo);
+        }
+    }
+
+    // Define a nested class that extends BroadcastReceiver
+    public class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Upon receiving each message from the wearable, display the following text
+            String receivedMessage = intent.getStringExtra("message");
+            messageText(receivedMessage);
+
+        }
     }
 }
--- wear/src/main/java/kr/co/episode/epilepsee/DrugCompleteActivity.java
+++ wear/src/main/java/kr/co/episode/epilepsee/DrugCompleteActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.Node;
@@ -25,6 +27,10 @@ import kr.co.episode.epilepsee.databinding.ActivityDrugCompleteBinding;
 public class DrugCompleteActivity extends Activity {
 
     private ActivityDrugCompleteBinding binding;
+
+    // data layer
+    int receivedMessageNumber = 1;
+    int sentMessageNumber=1;
 
     // 시간 출력
     SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");
@@ -45,49 +51,82 @@ public class DrugCompleteActivity extends Activity {
         // 기록된 시간 출력
         binding.textView8.setText(getTime);
 
-        //Use the same path//
-        String datapath = "/my_path";
-        new SendMessage(datapath, checkedRadio, getTime).start();
-    }
+        binding.textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String onClickMessage = "<약물 복용>\n"+checkedRadio + "\n복용 시간: " + getTime;
+                String path = "/my_path";
 
+                Task<List<Node>> nodeListTask = Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+                nodeListTask.addOnSuccessListener(new OnSuccessListener<List<Node>>() {
+                    @Override
+                    public void onSuccess(List<Node> nodes) {
+                        for (Node node : nodes) {
+                            Task<Integer> sendMessageTask = Wearable.getMessageClient(DrugCompleteActivity.this).sendMessage(node.getId(), path, onClickMessage.getBytes());
+                            sendMessageTask.addOnSuccessListener(new OnSuccessListener<Integer>() {
+                                @Override
+                                public void onSuccess(Integer result) {
+                                    // 메시지 전송 성공 시 수행할 작업
+                                }
+                            });
+                            sendMessageTask.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(Exception e) {
+                                    // 메시지 전송 실패 시 수행할 작업
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+//Register to receive local broadcasts, which we'll be creating in the next step//
+        IntentFilter newFilter = new IntentFilter(Intent.ACTION_SEND);
+        Receiver messageReceiver = new Receiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, newFilter);
+    }
+    public class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+//Display the following when a new message is received//
+            String onMessageReceived = "I just received a message from the handheld " + receivedMessageNumber++;
+            binding.textView9.setText(onMessageReceived);
+
+        }
+    }
     class SendMessage extends Thread {
         String path;
         String message;
-        String checkedRadio;
-        String getTime;
-
         //Constructor for sending information to the Data Layer//
-        SendMessage(String p, String checkedRadio, String getTime) {
+        SendMessage(String p, String m) {
             path = p;
-            this.checkedRadio = checkedRadio;
-            this.getTime = getTime;
+            message = m;
         }
-
         public void run() {
-            //Retrieve the connected devices//
+//Retrieve the connected devices//
             Task<List<Node>> nodeListTask =
                     Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
             try {
-                //Block on a task and get the result synchronously//
+//Block on a task and get the result synchronously//
                 List<Node> nodes = Tasks.await(nodeListTask);
                 for (Node node : nodes) {
-                    //Send the message///
-                    String fullMessage = checkedRadio + "\n복용 시간: " + getTime;
+//Send the message///
                     Task<Integer> sendMessageTask =
-                            Wearable.getMessageClient(DrugCompleteActivity.this).sendMessage(node.getId(), path, fullMessage.getBytes());
+                            Wearable.getMessageClient(DrugCompleteActivity.this).sendMessage(node.getId(), path, message.getBytes());
                     try {
                         Integer result = Tasks.await(sendMessageTask);
-                        //Handle the errors//
+//Handle the errors//
                     } catch (ExecutionException exception) {
-                        //TO DO//
+//TO DO//
                     } catch (InterruptedException exception) {
-                        //TO DO//
+//TO DO//
                     }
                 }
             } catch (ExecutionException exception) {
-                //TO DO//
+//TO DO//
             } catch (InterruptedException exception) {
-                //TO DO//
+//TO DO//
             }
         }
     }
